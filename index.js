e8e29c3546ad775e3bbdcf77b33583eb57213ed1
AronPelgrim <aron.pelgrim@hva.nl>
2022-03-21 14:15:40 +0100

    Added ejs files and partials

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const ejs = require('ejs')
 
-const router = require('./routes/router');
+const router = require('./routes/router')
+
+app.set('view engine', 'ejs')
+
+app.set('views', './views/pages')
+
+app.use(express.static('public'))
 
 app.use(router);
 
--- routes/router.js
+++ routes/router.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send('Hello World!')
-})
+    res.render('index');
+});
 
 router.use((req, res) => {
     res.status(404).send('Sorry, deze pagina bestaat niet');
