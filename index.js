af68ba1ff57a068b01dc2a514de45daa823f4709
AronPelgrim <aron.pelgrim@hva.nl>
2022-03-22 14:38:44 +0100

    Added ejs data and correct routing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000
 const ejs = require('ejs')
 
 const router = require('./routes/router')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
 app.set('view engine', 'ejs')
 
@@ -11,7 +12,7 @@ app.set('views', './views/pages')
 
 app.use(express.static('public'))
 
-app.use(router);
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,30 @@
-const express = require('express');
+const express = require('express')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('', (req, res) => {
-    res.render('index');
-});
+    res.render('index')
+})
+
+router.get('/schilderijen', (req, res) => {
+    fetchJson('https://www.rijksmuseum.nl/api/nl/collection?key=S0VK6DCj&q=rembrandt')
+    .then(function (jsonData) {
+    res.render('result', {
+      data: jsonData,
+    })
+  })
+})
 
 router.use((req, res) => {
-    res.status(404).send('Sorry, deze pagina bestaat niet');
-});
+    res.status(404).send('Sorry, deze pagina bestaat niet')
+})
 
-module.exports = router;
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .then((body) => body.artObjects) 
+    .catch((error) => error)
+}
+
+module.exports = router
